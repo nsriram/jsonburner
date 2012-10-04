7f4946ba4ccc7c2d2df41668f61807c25e359b6c
Sriram Narasimhan <narasis@thoughtworks.com>
2012-10-04 07:24:03 +0530

    Sriram:Adding more feeds and parameterized

--- feeds.rb
+++ feeds.rb
@@ -4,13 +4,24 @@ require 'json'
 require 'net/http'
 require 'active_support/core_ext'
 
-def thoughtblogs_rss
-  s = Net::HTTP.get_response(URI.parse('http://www.thoughtworks.com/blogs/rss/current')).body
+feeds = {"twblogs" => 'http://www.thoughtworks.com/blogs/rss/current',
+  "ilm" => 'http://feeds.feedburner.com/ILoveMadras?format=xml',
+  "ycombinator" => "http://news.ycombinator.com/rss",
+  "google" => "http://feeds.feedburner.com/blogspot/MKuf"}
+
+def thoughtblogs_rss(feed_url)
+  s = Net::HTTP.get_response(URI.parse(feed_url)).body
   Hash.from_xml(s).to_json
 end
 
 
-get '/latest.json' do
+get '/latest.json/:name' do
   content_type :json
-  thoughtblogs_rss  
+  puts feeds[params[:name]]
+  thoughtblogs_rss feeds[params[:name]]
+end
+
+get '/supported.json' do
+  content_type :json
+  feeds.to_json
 end
